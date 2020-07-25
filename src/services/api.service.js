063909c24976d9b0d9d4091f9606081dd826afef
Kanishka Mohan Madhuni <madhuni@Kanishkas-MacBook-Air.local>
2020-07-25 21:10:48 +0530

    chore(api.service): add functions to interect with server

---
+++ src/services/api.service.js
@@ -0,0 +1,41 @@
+const baseURL = 'https://api.github.com/users/supreetsingh247';
+
+/**
+ * This function will fetch the Github profile details of the user
+ *
+ * @returns {Promise<any>}
+ */
+export async function getUserProfile() {
+  const url = baseURL;
+  try {
+    const response = await fetch(url);
+    const data = response.json();
+    return Promise.resolve(data);
+  } catch (error) {
+    console.error(error);
+    alert('Something went wrong while fetching profile details.');
+    return Promise.resolve(null);
+  }
+}
+
+/**
+ * This function will fetch the Github repositories of the user.
+ *
+ * In this function catch and replace strategy is implemented. In case of any
+ * error occurs while fetching the repository details, we will show an alert
+ * message and then send the empty list back to handle the error gracefully.
+ *
+ * @returns {Promise<any[]>}
+ */
+export async function getRepoDetails() {
+  const url = `${baseURL}/repos`;
+  try {
+    const response = await fetch(url);
+    const data = response.json();
+    return Promise.resolve(data);
+  } catch (error) {
+    console.error(error);
+    alert('Something went wrong while fetching the repositories.');
+    return Promise.resolve([]);
+  }
+}
