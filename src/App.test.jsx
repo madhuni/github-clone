7a53d4f1614fc64db7a33b2eeaaadc43610ed7e7
Kanishka Mohan Madhuni <madhuni@Kanishkas-MacBook-Air.local>
2020-07-27 03:09:55 +0530

    chore: remove test

--- src/App.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('should create', () => {
-  const { getByText } = render(<App />);
-  const message = getByText(/App is running!/i);
-  expect(message).toBeInTheDocument();
-});
